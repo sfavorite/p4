d1eb805c791b1d183520d49a7deec00a71933587
Scot Favorite <scot.favorite@gmail.com>
2016-04-25 14:41:59 -0400

    Initial Commit

---
+++ app/Events/Event.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace AnswerMe\Events;
+
+abstract class Event
+{
+    //
+}
---
+++ app/Http/Requests/Request.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace AnswerMe\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+abstract class Request extends FormRequest
+{
+    //
+}
