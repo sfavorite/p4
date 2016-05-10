2b42c4fdf7733747b39e9bc8d37def905572ccc8
Scot Favorite <scot.favorite@gmail.com>
2016-05-10 12:31:51 -0400

    Testing migrations

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,14 +11,20 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+
         $this->call(UsersTableSeeder::class);
         $this->call(CountryTableSeeder::class);
         $this->call(CityTableSeeder::class);
-        $this->call(QuestionTableSeeder::class);
-        $this->call(ProfilesTableSeeder::class);
-        $this->call(PossibilitiesTableSeeder::class);
         $this->call(CategoryTableSeeder::class);
+        echo 'QuestionTable' . PHP_EOL;
+        $this->call(QuestionTableSeeder::class);
+        echo 'Profiles' . PHP_EOL;
+        $this->call(ProfilesTableSeeder::class);
+        echo 'Possibilities' . PHP_EOL;
+        $this->call(PossibilitiesTableSeeder::class);
+        echo 'QuestionUser' . PHP_EOL;
         $this->call(QuestionUserTableSeeder::class);
+        echo 'PossibilityQuestion' . PHP_EOL;
         $this->call(PossibilityQuestionTableSeeder::class);
         $this->call(OpinionsTableSeeder::class);
     }
