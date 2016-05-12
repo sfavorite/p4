48e888c78b17d1ccd1c9a6c90ea07b2d429a1e67
Scot Favorite <scot.favorite@gmail.com>
2016-05-12 13:21:32 -0400

    Changes based on validator.w3.org

--- resources/views/signup/signup.blade.php
+++ resources/views/signup/signup.blade.php
@@ -5,8 +5,8 @@
     <div class="text-center">
 
         <h3>Already a member. Login and get started!<br>
-            <div class="button-spacer"></div>
         </h3>
+        <div class="button-spacer"></div>
 
         <div class="row">
             <div class="col-md-3"></div>
@@ -26,7 +26,7 @@
             <div class="col-md-3"></div>
                 <div class="col-md-6">
 
-                    <h3>Wait...what? You aren't a member yet?</br>
+                    <h3>Wait...what? You aren't a member yet?</h3></br>
                         <div class="button-spacer"></div>
                         Sign up now and join us!
                     </h3>
